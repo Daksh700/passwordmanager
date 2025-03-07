49091fddeea07cef08b9eca05857e649fb7fa077
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2025-03-07 15:09:01 +0530

    fixed google auth

--- backend/src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { googleLogin } = require("../controller/authController");
-
-const router = express.Router();
-
-router.post("/google", googleLogin);
-
-module.exports = router;
