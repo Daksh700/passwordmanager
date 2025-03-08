e4e62417f3615f8d37811abb45723f76a1eaafa8
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2025-03-09 00:34:31 +0530

    navbar links updated

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -18,21 +18,23 @@ const Navbar = () => {
     return(
         <nav className="py-6 px-8">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <a href="/" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
           <div className="text-blue-600 text-4xl font-bold font-micro5">
             PASSKEY
           </div>
+          </a>
           <div className="space-x-8">
-            <a href="#" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
+            <a href="/" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
               Home
             </a>
-            <a href="#" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
+            <a href="/about" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
               About
             </a>
-            <a href="#" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
+            <a href="/faqs" className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
               FAQs
             </a>
             {location.pathname !== '/login' && (
-                <a onClick={handleLogout} className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15">
+                <a onClick={handleLogout} className="text-blue-600 hover:text-blue-700 text-3xl font-jersey15 hover:cursor-pointer">
                     Logout
                 </a>
             )}
